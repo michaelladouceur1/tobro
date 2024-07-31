e6e9846ff4ec6d5a0a7817223e5f984454fffa95
michaelladouceur1 <michaelladouceurdev@gmail.com>
2024-07-30 22:57:06 -0500

    updated config view in main card

---
+++ web/src/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme} from "@mui/material/styles";
+
+
+export const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#549F93",
+      },
+      secondary: {
+        main: "#28536B",
+      },
+      text: {
+        primary: "#F5E0B7",
+      },
+      error: {
+        main: "#FB6107",
+      },
+      background: {
+        default: "#0C1618",
+      },
+    },
+    components: {
+      MuiCard: {
+        styleOverrides: {
+          root: {
+            backgroundColor: "#1B2F33",
+            color: "#F5E0B7",
+          },
+        },
+      },
+    },
+  });
