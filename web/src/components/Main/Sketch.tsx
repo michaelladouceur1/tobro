d20e19d49b38e8187ff57cb629fde82490124e6a
michaelladouceur1 <michaelladouceurdev@gmail.com>
2024-08-26 23:25:58 -0500

    added speed dial to sketch menu

--- web/src/components/Main/Sketch.tsx
+++ web/src/components/Main/Sketch.tsx
@@ -1,4 +1,48 @@
-import {Box, styled} from "@mui/material";
+import {
+  Box,
+  SpeedDial,
+  SpeedDialAction,
+  SpeedDialIcon,
+  styled,
+} from "@mui/material";
+import {FaPlus, FaSave} from "react-icons/fa";
+import {useHttpApi} from "../../hooks/useHttpApi";
+
+function SketchSpeedDial() {
+  const api = useHttpApi();
+
+  const actions = [
+    {
+      icon: <FaPlus />,
+      name: "New Sketch",
+      onclick: () => console.log("New Sketch"),
+    },
+    {
+      icon: <FaSave />,
+      name: "Save Sketch",
+      onclick: () => console.log("Save Sketch"),
+    },
+  ];
+
+  return (
+    <SpeedDial
+      ariaLabel="Speedial"
+      sx={{position: "absolute", right: "20px", top: "20px"}}
+      transitionDuration={0}
+      icon={<SpeedDialIcon />}
+      direction="left"
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          key={action.name}
+          icon={action.icon}
+          tooltipTitle={action.name}
+          onClick={action.onclick}
+        />
+      ))}
+    </SpeedDial>
+  );
+}
 
 export function Sketch() {
   const Sketch = styled(Box)({
@@ -8,8 +52,10 @@ export function Sketch() {
   });
 
   return (
-    <Sketch className="sketch-view">
+    <Sketch className="sketch-view" sx={{position: "relative"}}>
+      <SketchSpeedDial />
       <img src="/images/sketch.png" alt="sketch" />
+      <h1>SKETCH</h1>
     </Sketch>
   );
 }
